e2b80002deef0462afa6eb1f12375cc75b88f553
agatha-duzan <agathaduzan@gmail.com>
2023-12-08 13:26:28 +0100

    added multi dim feature NN

--- nn_helpers.py
+++ nn_helpers.py
@@ -24,10 +24,6 @@ def print_metrics(model_name, y_true, y_pred):
     print(f'Recall: {round(skm.recall_score(y_true, y_pred), 2)}')
     print(f'F1 score: {round(skm.f1_score(y_true, y_pred), 2)}')
 
-def sliding_window_iterator(df, window_size=8, overlap=7):
-    for i in range(0, len(df) - window_size + 1, overlap):
-        yield df[i:i + window_size]
-
 def create_series(X, n):
     """
     Create a 2D array where each row is a sliding window view of array X with length n.
@@ -44,6 +40,28 @@ def create_series(X, n):
 
     for i in range(num_windows):
         series[i, :] = X[i:i + n]
+
+    return series
+
+def create_series_dim(X, n):
+    """
+    Create a 2D array where each row is a sliding window view of array X with length m*n.
+
+    Parameters:
+    - X: m-dimensional array
+    - n: int, length of the sliding window
+
+    Returns:
+    - 2D array
+    """
+    num_windows = len(X) - n + 1
+    series = np.zeros((num_windows, n, X.shape[1]), dtype=X.dtype)
+
+    for i in range(num_windows):
+        series[i, :] = X[i:i + n]
+
+    # Flatten the series
+    series = series.reshape(num_windows, -1)
 
     return series
 
@@ -75,14 +93,17 @@ def neural_net(X, y, window_size, epochs=10, batch_size=32, with_class_weights=F
     test_metrics[4] = test_metrics[4][0] # F1 score is an array of one elemet
     return test_metrics
 
-def neural_net_comp(X, y, window_sizes, epochs=10, batch_size=32, with_class_weights=False):
+def neural_net_comp(X, y, window_sizes, epochs=10, batch_size=32, dim=False, with_class_weights=False):
     '''
     Compares the performance of the neural network with different window sizes
     for each window, we get info on : loss, accuracy, precision, recall, f1 score
     '''
     metrics_array = []
     for window_size in window_sizes:
-        test_metrics = neural_net(X, y, window_size, with_class_weights)
+        if dim:
+            test_metrics = neural_net_dim(X, y, window_size, epochs, batch_size, with_class_weights)
+        else:
+            test_metrics = neural_net(X, y, window_size, with_class_weights)
         metrics_array.append(test_metrics)
     return metrics_array
 
@@ -118,24 +139,48 @@ def plot_metrics(metrics_array, window_sizes):
     plt.title('Precision, Recall, and F1 Score')
     plt.show()
 
-def plot_improvement(test_metrics_unbalanced, test_metrics_balanced):
+def print_improvement(test_metrics_1, test_metrics_2, improvement_name):
     '''
-    Plots the improvement of the model with class weights
+    Prints the improvement of the model with 'improvement_name'
     '''
-def plot_histogram(data, title):
-    sns.set_style('whitegrid')
-    sns.set_palette('Set2')
-    plt.hist(data, bins=10)
-    plt.title(title)
-    plt.show()
+    print(f'Performance of the model with {improvement_name}:')
+    print(f'Loss: {round(test_metrics_1[0], 2)}')
+    print(f'Accuracy: {round(test_metrics_1[1], 2)}')
+    print(f'Precision: {round(test_metrics_1[2], 2)}')
+    print(f'Recall: {round(test_metrics_1[3], 2)}')
+    print(f'F1 score: {round(test_metrics_1[4], 2)}')
+    print(f'\nPerformance difference of the model with {improvement_name}:')
+    print(f'Loss: {round(test_metrics_1[0] - test_metrics_2[0], 2)}')
+    print(f'Accuracy: {round(test_metrics_1[1] - test_metrics_2[1], 2)}')
+    print(f'Precision: {round(test_metrics_1[2] - test_metrics_2[2], 2)}')
+    print(f'Recall: {round(test_metrics_1[3] - test_metrics_2[3], 2)}')
+    print(f'F1 score: {round(test_metrics_1[4] - test_metrics_2[4], 2)}')
 
-# Plot Loss Histogram
-plot_histogram(metrics_df['loss'], 'Loss')
+def neural_net_dim(X, y, window_size, epochs=10, batch_size=32, with_class_weights=False):
+    X1_series = create_series_dim(X, window_size) # each line is a series of length window_size*number of features
+    y1 = y[window_size-1:].astype('float32') # the label is the last element of the series
 
-# Plot Accuracy Histogram
-plot_histogram(metrics_df['accuracy'], 'Accuracy')
+    # split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X1_series, y1, test_size=0.2, random_state=42)
+    
+    # define the neural network
+    model_1 = models.Sequential()
+    model_1.add(layers.Dense(128, activation='relu', input_shape=(window_size*X.shape[1],)))
+    model_1.add(layers.Dense(64, activation='relu'))
+    model_1.add(layers.Dense(32, activation='relu'))
+    model_1.add(layers.Dense(1, activation='sigmoid'))
 
-# Plot Precision, Recall, and F1 Score Histogram
-plot_histogram(metrics_df['precision'], 'Precision')
-plot_histogram(metrics_df['recall'], 'Recall')
-plot_histogram(metrics_df['f1 score'], 'F1 Score')
+    # compile the model and fit it to the training data
+    model_1.compile(optimizer='adam', loss='binary_crossentropy', 
+                    metrics=['accuracy', tf.keras.metrics.Precision(), tf.keras.metrics.Recall(), tf.keras.metrics.F1Score()])
+    
+    if not with_class_weights : #default
+        model_1.fit(X_train, y_train, epochs=epochs, batch_size=batch_size)
+    else : # mitigate class imbalance with class weights
+        class_weights = class_weight.compute_class_weight('balanced', classes=np.unique(y_train), y=y_train)
+        model_1.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, class_weight=class_weights)
+    
+    # Evaluate the model on the test set
+    test_metrics = model_1.evaluate(X_test, y_test)
+    test_metrics[4] = test_metrics[4][0] # F1 score is an array of one elemet
+    return test_metrics
